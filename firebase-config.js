a56400818e1315980029a996bb08778d2d5354ed
rigelcoid1512-collab <rigel.co.id.1512@gmail.com>
2025-08-20 15:11:05 +0700

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Ganti dengan konfigurasi Firebase kamu
+const firebaseConfig = {
+  apiKey: "AIzaSyARn4Qc1aK6m2sfuKxzuWtGIHIQHPdeAH0",
+  authDomain: "chat-anonim-712c5.firebaseapp.com",
+  projectId: "chat-anonim-712c5",
+  storageBucket: "chat-anonim-712c5.firebasestorage.app",
+  messagingSenderId: "488878022911",
+  appId: "1:488878022911:web:18808cd29505aa0720acca"
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
